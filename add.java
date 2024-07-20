93b26027eee49089ec12313e0b032a726f246367
Ravindra Kumar <119472779+ravindrakumar08696@users.noreply.github.com>
2024-07-20 14:06:37 +0530

    Add files via upload
    This is just from the upload.

---
+++ add.java
@@ -0,0 +1,15 @@
+
+public class add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum=0;
+		int a=12;
+		int b=23;
+		sum=a+b;
+		System.out.println("The sum of two numbers are :"+sum);
+		
+		
+	}
+
+}
